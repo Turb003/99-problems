4249945859be09dc96c8cbef60a3c3f0d56a44c5
Peter Urbak <peter@dragonwasrobot.com>
2014-07-12 12:06:35 +0200

    Finished Q24.

--- ninetynine.scm
+++ ninetynine.scm
@@ -372,11 +372,14 @@
 ;; 24. (*) Lotto: Draw N different random numbers from the set 1..M.
 
 ;; The selected numbers shall be returned in a list.
-;; Example:
-;; * (lotto-select 6 49)
-;; (23 1 17 33 21 37)
 
 ;; Hint: Combine the solutions of problems P22 and P23.
+
+(define (my-lotto-select n m)
+  (my-random-select (my-range 1 m) n))
+
+(my-lotto-select 6 49)
+;; ==> (5 11 36 40 43 46)
 
 ;; 25. (*) Generate a random permutation of the elements of a list.
 
